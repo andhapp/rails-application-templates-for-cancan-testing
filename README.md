b713c7fd00a6a8d3a03cf9fdf8e386f7014841b8
Anuj Dutta <anuj@andhapp.com>
2012-06-13 01:18:44 +0100

    First commit.

---
+++ README.md
@@ -0,0 +1,49 @@
+# Rails application templates for cancan testing
+
+The title says it all. This is just a couple of templates to create
+quick projects with CanCan only for testing purposes. I hope to make it
+easy for bug reporters to create dummy apps so that it's easier for
+project maintainers to investigate some of the tricky issues. 
+
+
+# Templates
+
+## ActiveRecord
+This is for creating a cancan application with activerecord as ORM. It
+uses sqlite3 as the default ORM.
+
+## Mongoid
+This is for creating a cancan application with mongoid as ORM. Please
+note that when creating the application one should use
+--skip-active-record explicitly.
+ 
+## Datamapper
+
+Still need to do this. Haven't really seen any datamapper issues. Is
+anyone using datamapper now days?
+
+
+# Usage
+
+## ActiveRecord
+
+Please run the following command:
+
+<pre>
+rails new cancan-test --skip-bundle -m https://raw.github.com/andhapp/rails-application-templates-for-cancan-testing/master/activerecord-application-template.rb 
+</pre>
+
+
+## Mongoid
+
+Please run the following command:
+
+<pre>
+rails new cancan-test --skip-active-record --skip-bundle -m https://raw.github.com/andhapp/rails-application-templates-for-cancan-testing/master/activerecord-application-template.rb 
+</pre>
+
+
+# Models
+
+It creates two models, namely, User and Company since this is just going
+to be a small app for replicating the issues.
---
+++ mongoid-application-template.rb
@@ -0,0 +1,83 @@
+run "rm public/index.html"
+run "rm public/images/rails.png"
+run "touch tmp/.gitignore log/.gitignore"
+
+run "rm -f .gitignore"
+file ".gitignore", <<-END.gsub(/  /, '')
+  .DS_Store
+  log/*.log
+  tmp/**/*
+  config/database.yml
+  db/*.sqlite3
+  vendor/ruby
+END
+
+git :init
+git :add => '.'
+git :commit => "-m 'Initial commit: Clean application'"
+
+
+puts
+say_status  "Rubygems", "Adding Rubygems into Gemfile...\n", :yellow
+puts        '-'*80, ''; sleep 1
+
+local_cancan_path = ask("If you want to use local checked out repository for CanCan, then please enter the path to it, else leave blank?")
+
+if local_cancan_path == ""
+  gem "cancan"
+else
+  gem "cancan", path: "#{local_cancan_path}"
+end
+
+gem 'mongoid', '2.4.11'
+gem 'bson_ext'
+
+git :add => '.'
+git :commit => "-m 'Added gems'"
+
+
+puts
+say_status  "Rubygems", "Installing Rubygems...", :yellow
+puts        '-'*80, ''
+
+run "bundle install --path=vendor"
+
+
+puts
+say_status  "Mongoid", "Adding mongo database connection file...", :yellow
+puts        '-'*80, ''; sleep 1
+
+file 'config/mongoid.yml', <<-CODE
+development:
+  host: localhost
+  database: cancan_test_development
+CODE
+
+
+puts
+say_status  "Model", "Adding the User resource...", :yellow
+puts        '-'*80, ''; sleep 1
+
+generate :scaffold, "User username:string password:string"
+rake  "db:migrate"
+
+git :add => '.'
+git :commit => "-m 'Added the User resource'"
+
+
+puts
+say_status  "Model", "Adding the Company resource...", :yellow
+puts        '-'*80, ''; sleep 1
+
+generate :scaffold, "Company name:string"
+rake  "db:migrate"
+
+git :add => '.'
+git :commit => "-m 'Added the Company resource'"
+
+
+puts
+say_status  "Ability", "Adding the ability model...", :yellow
+puts        '-'*80, ''; sleep 1
+
+generate 'cancan:ability'
